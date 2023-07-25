d6af623ce5f9623aaa04c8bdf0189ca9a57ac679
gayathrikrishnamoorthi <139099565+gayathrikrishnamoorthi@users.noreply.github.com>
2023-07-25 09:55:38 +0530

    Add files via upload

---
+++ Coin.java
@@ -0,0 +1,37 @@
+package gayathri;
+
+import java.util.Scanner;
+
+public class Coin {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++) {
+            arr[i] = sc.nextInt();
+        }
+        int result = sc.nextInt();
+        System.out.println(findSol(arr, n, result));
+        sc.close();
+    }
+
+    static int findSol(int[] arr,int n,int result) {
+        int sum = 0,count = 0;
+        for(int i=n-1;i>=0;i--) {
+            while(sum <= result) {
+                if(sum + arr[i] <= result) {
+                    sum += arr[i];
+                    count++;
+                } else {
+                    break;
+                }
+            }
+            if(sum == result) {
+                return count;
+            }
+        }
+        return -1;
+    }
+}
+
+
